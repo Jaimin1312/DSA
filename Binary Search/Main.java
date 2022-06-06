82b3f547af8eb82ec4dddb76d946ac59cbdfe6fe
Jaimin <jaimin@tazapay.com>
2022-06-06 13:05:35 +0530

    binary serach

---
+++ Binary Search/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public  static  void  main(String[] args){
+        int[] arr = {10,20,30,40,50};
+        int target = 20;
+        BinarySerach bs = new BinarySerach();
+        System.out.println(bs.Search(arr,target));
+    }
+}
